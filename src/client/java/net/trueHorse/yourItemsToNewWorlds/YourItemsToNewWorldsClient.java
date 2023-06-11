c455bb822fa1c3afa4c84903d8f741124c71750c
Paul Roß <81418739+TrueHorse@users.noreply.github.com>
2023-06-11 22:03:07 +0200

    replace template placeholders

--- src/client/java/net/trueHorse/yourItemsToNewWorlds/YourItemsToNewWorldsClient.java
+++ src/client/java/net/trueHorse/yourItemsToNewWorlds/YourItemsToNewWorldsClient.java
@@ -1,8 +1,8 @@
-package com.example;
+package net.trueHorse.yourItemsToNewWorlds;
 
 import net.fabricmc.api.ClientModInitializer;
 
-public class ExampleModClient implements ClientModInitializer {
+public class YourItemsToNewWorldsClient implements ClientModInitializer {
     @Override
     public void onInitializeClient() {
         // This entrypoint is suitable for setting up client-specific logic, such as rendering.
