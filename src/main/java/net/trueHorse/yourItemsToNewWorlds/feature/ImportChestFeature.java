5eea6174a2b45c0782a311bc54e7657ff7dc6867
Paul Roß <81418739+TrueHorse@users.noreply.github.com>
2023-07-17 17:26:50 +0200

    generating chests with correct items

--- src/main/java/net/trueHorse/yourItemsToNewWorlds/feature/ImportChestFeature.java
+++ src/main/java/net/trueHorse/yourItemsToNewWorlds/feature/ImportChestFeature.java
@@ -1,54 +1,72 @@
 package net.trueHorse.yourItemsToNewWorlds.feature;
 
 import com.mojang.serialization.Codec;
-import it.unimi.dsi.fastutil.ints.IntArrayList;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.Blocks;
-import net.minecraft.block.entity.LootableContainerBlockEntity;
-import net.minecraft.loot.LootTables;
-import net.minecraft.util.Util;
+import net.minecraft.block.ChestBlock;
+import net.minecraft.block.entity.ChestBlockEntity;
+import net.minecraft.item.ItemStack;
 import net.minecraft.util.math.BlockPos;
-import net.minecraft.util.math.ChunkPos;
-import net.minecraft.util.math.Direction;
-import net.minecraft.util.math.random.Random;
 import net.minecraft.world.Heightmap;
 import net.minecraft.world.StructureWorldAccess;
 import net.minecraft.world.gen.feature.DefaultFeatureConfig;
 import net.minecraft.world.gen.feature.Feature;
 import net.minecraft.world.gen.feature.util.FeatureContext;
 
-import java.util.stream.IntStream;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Predicate;
 
 public class ImportChestFeature extends Feature<DefaultFeatureConfig> {
+
+    public static ArrayList<ItemStack> importItems = new ArrayList<>();
     public ImportChestFeature(Codec<DefaultFeatureConfig> configCodec) {
         super(configCodec);
     }
 
     @Override
     public boolean generate(FeatureContext<DefaultFeatureConfig> context) {
-        Random random = context.getRandom();
         StructureWorldAccess structureWorldAccess = context.getWorld();
-        ChunkPos chunkPos = new ChunkPos(context.getOrigin());
-        IntArrayList intArrayList = Util.shuffle(IntStream.rangeClosed(chunkPos.getStartX(), chunkPos.getEndX()), random);
-        IntArrayList intArrayList2 = Util.shuffle(IntStream.rangeClosed(chunkPos.getStartZ(), chunkPos.getEndZ()), random);
-        BlockPos.Mutable mutable = new BlockPos.Mutable();
-        for (Integer integer : intArrayList) {
-            for (Integer integer2 : intArrayList2) {
-                mutable.set(integer, 0, integer2);
-                BlockPos blockPos = structureWorldAccess.getTopPosition(Heightmap.Type.MOTION_BLOCKING_NO_LEAVES, mutable);
-                if (!structureWorldAccess.isAir(blockPos) && !structureWorldAccess.getBlockState(blockPos).getCollisionShape(structureWorldAccess, blockPos).isEmpty()) continue;
-                structureWorldAccess.setBlockState(blockPos, Blocks.CHEST.getDefaultState(), Block.NOTIFY_LISTENERS);
-                LootableContainerBlockEntity.setLootTable(structureWorldAccess, random, blockPos, LootTables.SPAWN_BONUS_CHEST);
-                BlockState blockState = Blocks.DIAMOND_BLOCK.getDefaultState();
-                for (Direction direction : Direction.Type.HORIZONTAL) {
-                    BlockPos blockPos2 = blockPos.offset(direction);
-                    if (!blockState.canPlaceAt(structureWorldAccess, blockPos2)) continue;
-                    structureWorldAccess.setBlockState(blockPos2, blockState, Block.NOTIFY_LISTENERS);
+        BlockPos origin = context.getOrigin();
+        int chestCount = (int)Math.ceil(importItems.size()/27.0);
+
+        BlockPos.Mutable mutable = origin.mutableCopy();
+        mutable.setZ(mutable.getZ()+2);
+        while (true) {
+            mutable.setZ(mutable.getZ() + 1);
+            ArrayList<BlockPos> blockPositions = this.getGenerationPositions(mutable,chestCount,structureWorldAccess);
+            if(blockPositions.stream().map(blockPos -> !structureWorldAccess.isAir(blockPos) && !structureWorldAccess.getBlockState(blockPos).getCollisionShape(structureWorldAccess, blockPos).isEmpty()).noneMatch(Predicate.isEqual(true))){
+                for (int i = 0;i<blockPositions.size();i++) {
+                    BlockState blockState = Blocks.CHEST.getDefaultState();
+                    ChestBlockEntity blockEntity = (ChestBlockEntity) ((ChestBlock) Blocks.CHEST).createBlockEntity(blockPositions.get(i), blockState);
+                    List<ItemStack> chestContent = importItems.subList(i*27,importItems.size()-i*27<27?importItems.size():(i+1)*27);
+                    for(int j = 0;j<chestContent.size();j++){
+                        blockEntity.setStack(j,chestContent.get(j));
+                    }
+
+                    structureWorldAccess.setBlockState(blockPositions.get(i), blockState, Block.NOTIFY_LISTENERS);
+                    structureWorldAccess.getChunk(blockPositions.get(i)).setBlockEntity(blockEntity);
                 }
                 return true;
             }
         }
-        return false;
+    }
+
+    public ArrayList<BlockPos> getGenerationPositions(BlockPos pos, int chestCount, StructureWorldAccess structureWorldAccess){
+        ArrayList<BlockPos> blockPositions = new ArrayList<>();
+        BlockPos blockPos = structureWorldAccess.getTopPosition(Heightmap.Type.MOTION_BLOCKING_NO_LEAVES, pos);
+        int chestStackHeight = (int) Math.ceil(chestCount / 2.0);
+        int addedPositions = 0;
+        for (int i = 0; i < chestStackHeight; i++) {
+            for (int j = 0; j < 2; j++) {
+                blockPositions.add(new BlockPos(blockPos.getX()+j, blockPos.getY() + i, blockPos.getZ()));
+                addedPositions++;
+                if(addedPositions==chestCount){
+                    break;
+                }
+            }
+        }
+        return blockPositions;
     }
 }
--- src/main/java/net/trueHorse/yourItemsToNewWorlds/mixin/MinecraftServerMixin.java
+++ src/main/java/net/trueHorse/yourItemsToNewWorlds/mixin/MinecraftServerMixin.java
@@ -1,5 +1,6 @@
 package net.trueHorse.yourItemsToNewWorlds.mixin;
 
+import net.minecraft.item.ItemStack;
 import net.minecraft.server.MinecraftServer;
 import net.minecraft.server.world.ServerWorld;
 import net.minecraft.util.math.BlockPos;
@@ -7,11 +8,14 @@ import net.minecraft.world.gen.feature.FeatureConfig;
 import net.minecraft.world.level.LevelProperties;
 import net.minecraft.world.level.ServerWorldProperties;
 import net.trueHorse.yourItemsToNewWorlds.duck.GeneratorOptionsAccess;
+import net.trueHorse.yourItemsToNewWorlds.feature.ImportChestFeature;
 import net.trueHorse.yourItemsToNewWorlds.feature.YourItemsToNewWorldsFeatures;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import java.util.ArrayList;
 
 @Mixin(MinecraftServer.class)
 public class MinecraftServerMixin {
@@ -19,7 +23,9 @@ public class MinecraftServerMixin {
     @Inject(method = "setupSpawn",at=@At("TAIL"))
     private static void setupImportChests(ServerWorld world, ServerWorldProperties worldProperties, boolean bonusChest, boolean debugWorld, CallbackInfo ci){
         if(debugWorld) return;
-        if(!((GeneratorOptionsAccess)((LevelProperties)worldProperties).getGeneratorOptions()).getImportItems().isEmpty()){
+        ArrayList<ItemStack> importItems = ((GeneratorOptionsAccess)((LevelProperties)worldProperties).getGeneratorOptions()).getImportItems();
+        if(!importItems.isEmpty()){
+            ImportChestFeature.importItems = importItems;
             YourItemsToNewWorldsFeatures.importChestsFeature.generateIfValid(FeatureConfig.DEFAULT,world, world.getChunkManager().getChunkGenerator(), world.random, new BlockPos(worldProperties.getSpawnX(), worldProperties.getSpawnY(), worldProperties.getSpawnZ()));
         }
     }
