06d2404916e55d5b4e79de3728907edc4cfd2331
Paul Roß <81418739+TrueHorse@users.noreply.github.com>
2023-08-10 13:20:30 +0200

    Revert "optimized cache a bit"
    This reverts commit bb95d59241ac64367a5ecceba922c0549f7409b1.

--- src/client/java/net/trueHorse/yourItemsToNewWorlds/gui/handlers/ItemSearchConfig.java
+++ src/client/java/net/trueHorse/yourItemsToNewWorlds/gui/handlers/ItemSearchConfig.java
@@ -1,8 +1,9 @@
 package net.trueHorse.yourItemsToNewWorlds.gui.handlers;
 
-import net.minecraft.util.math.ChunkPos;
+import net.minecraft.util.math.BlockPos;
+import net.trueHorse.yourItemsToNewWorlds.io.ItemImporter;
 
 import java.nio.file.Path;
 
-public record ItemSearchConfig(Path worldPath, String playerName, ChunkPos searchChunk, int searchRadius) {
+public record ItemSearchConfig(Path worldPath, String playerName, ItemImporter.SearchLocationDeterminationMode searchLocationDeterminationMode, BlockPos searchCoords, int searchRadius) {
 }
