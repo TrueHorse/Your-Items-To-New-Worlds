bb95d59241ac64367a5ecceba922c0549f7409b1
Paul Roß <81418739+TrueHorse@users.noreply.github.com>
2023-08-10 13:19:45 +0200

    optimized cache a bit

--- src/client/java/net/trueHorse/yourItemsToNewWorlds/gui/handlers/ItemSearchConfig.java
+++ src/client/java/net/trueHorse/yourItemsToNewWorlds/gui/handlers/ItemSearchConfig.java
@@ -1,9 +1,8 @@
 package net.trueHorse.yourItemsToNewWorlds.gui.handlers;
 
-import net.minecraft.util.math.BlockPos;
-import net.trueHorse.yourItemsToNewWorlds.io.ItemImporter;
+import net.minecraft.util.math.ChunkPos;
 
 import java.nio.file.Path;
 
-public record ItemSearchConfig(Path worldPath, String playerName, ItemImporter.SearchLocationDeterminationMode searchLocationDeterminationMode, BlockPos searchCoords, int searchRadius) {
+public record ItemSearchConfig(Path worldPath, String playerName, ChunkPos searchChunk, int searchRadius) {
 }
