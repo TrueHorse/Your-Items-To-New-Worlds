2fab9aaf2d0edfb7ed3496116129e316bbb4d78e
Paul Roß <81418739+TrueHorse@users.noreply.github.com>
2023-08-25 15:56:57 +0200

    fixed button not showing up

--- src/main/java/net/trueHorse/yourItemsToNewWorlds/mixin/CreateWorldScreenMoreTabMixin.java
+++ src/main/java/net/trueHorse/yourItemsToNewWorlds/mixin/CreateWorldScreenMoreTabMixin.java
@@ -7,6 +7,7 @@ import net.minecraft.client.gui.screens.worldselection.CreateWorldScreen;
 import net.minecraft.network.chat.Component;
 import net.trueHorse.yourItemsToNewWorlds.duck.WorldCreatorAccess;
 import net.trueHorse.yourItemsToNewWorlds.gui.ImportItemsScreen;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.Unique;
@@ -15,11 +16,12 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
 
-@Mixin(targets = "net/minecraft/client/gui/screens/CreateWorldScreen$MoreTab")
+@Mixin(targets = "net/minecraft/client/gui/screens/worldselection/CreateWorldScreen$MoreTab")
 public class CreateWorldScreenMoreTabMixin {
 
+    @Final
     @Shadow
-    private CreateWorldScreen this$0;
+    CreateWorldScreen this$0;
     @Unique
     private ImportItemsScreen oldImportItemScreen = null;
 
